b88b4088899fdeb5c6dbb2699270149f4fd47594
axelt <axel.teyssier31@gmail.com>
2024-04-06 19:19:10 +0200

    fix: i fix the lastest bug and routing error.

--- project_app/apps.py
+++ project_app/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class ProjectAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'project_app'
-
-    @staticmethod
-    def ready():
-        import project_app.signals
